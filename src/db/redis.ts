ea8b23c4d5da3680ca57f7fcccdfc9a70fb884c1
Prachi Kedia <prachikedia2005@gmail.com>
2026-03-15 00:31:53 +0530

    added upstash redis support

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,7 +1,11 @@
 import Redis from "ioredis";
 
-export const redisConnection = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
-  maxRetriesPerRequest: null
-}); 
+export const redisConnection = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null
+    })
+  : new Redis({
+      host: "127.0.0.1",
+      port: 6379,
+      maxRetriesPerRequest: null
+    });
