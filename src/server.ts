fa6130388d79c9f0124e6843da7f6a8363c64dba
Prachi Kedia <prachikedia2005@gmail.com>
2026-03-14 23:19:19 +0530

    fixed port issue

--- src/server.ts
+++ src/server.ts
@@ -19,9 +19,11 @@ const start = async () => {
   try {
     await connectDB();
 
-    await app.listen({ port: 3000 });
+   const PORT = Number(process.env.PORT) || 3000;
 
-    console.log("Server running on port 3000");
+await app.listen({ port: PORT, host: "0.0.0.0" });
+
+console.log(`Server running on port ${PORT}`);
   } catch (err) {
     console.error(err);
     process.exit(1);
